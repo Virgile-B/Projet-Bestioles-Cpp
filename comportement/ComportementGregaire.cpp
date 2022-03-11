0143f6ec9a2b23a40aae4692ee35089b5d6c4567
QAvare <quentin.avare@gmail.com>
2022-03-11 16:59:00 +0100

    gregaire qui marche (un peu)

--- comportement/ComportementGregaire.cpp
+++ comportement/ComportementGregaire.cpp
@@ -18,12 +18,15 @@ ComportementGregaire::ComportementGregaire(void){
     couleur[ 2 ] = 0;
 }
 
-void ComportementGregaire::action(Bestiole actualBestiole, Milieu &monMilieu){
+void ComportementGregaire::action(Bestiole *actualBestiole, Milieu &monMilieu){
     float dir_moy = 0;
-    std::vector<Bestiole> mesVoisins = monMilieu.Voisins(actualBestiole);
-    for ( std::vector<Bestiole>::iterator it = mesVoisins.begin() ; it != mesVoisins.end() ; ++it ){
+    std::cout << actualBestiole->get_orientation() << std::endl;
+    std::vector<Bestiole> mesVoisins = monMilieu.Voisins(*actualBestiole);
+    if (mesVoisins.size() != 0){
+        for ( std::vector<Bestiole>::iterator it = mesVoisins.begin() ; it != mesVoisins.end() ; ++it ){
         dir_moy += (it->get_orientation());
     }
     actualBestiole->set_orientation(dir_moy/(mesVoisins.size()));
+    }
 }
 
